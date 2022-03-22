f9465854d6060551a463b19750a8f40d3dd762df
mujammelhoque <mujammelh111@gmail.com>
2022-03-22 16:00:49 +0600

    controller and route changes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('dashboard','AdminController@dashboard'); 
+});
